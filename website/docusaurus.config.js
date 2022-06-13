676835029ec228752c69e4fd27ebc94e9eaeece0
Huy Nguyen <Smilefounder@users.noreply.github.com>
2022-06-13 13:21:40 +0700

    upd

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,122 +1,118 @@
 module.exports = {
-  title: 'Mixcore CMS',
-  tagline: 'Fully Open Source ASP.Net Core / Dotnet Core CMS UI Toolkit',
-  url: 'https://mixcore.org',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'mixcore', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
-  onBrokenLinks: 'ignore',
-  themeConfig: {
-    navbar: {
-      title: 'Mixcore CMS',
-      logo: {
-        alt: 'Mixcore CMS Logo',
-        src: 'img/logo.png',
-      },
-      items: [
-        // {to: '/', label: 'Docs', position: 'left'},
-        // {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://mixcore.org',
-          label: 'Home',
-          position: 'right',
+    title: 'Mixcore CMS',
+    tagline: 'Fully Open Source ASP.Net Core / Dotnet Core CMS UI Toolkit',
+    url: 'https://mixcore.org',
+    baseUrl: '/',
+    favicon: 'img/favicon.ico',
+    organizationName: 'mixcore', // Usually your GitHub org/user name.
+    projectName: 'docs', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
+    onBrokenLinks: 'ignore',
+    themeConfig: {
+        navbar: {
+            title: 'Mixcore CMS',
+            logo: {
+                alt: 'Mixcore CMS Logo',
+                src: 'img/logo.png',
+            },
+            items: [
+                // {to: '/', label: 'Docs', position: 'left'},
+                // {to: 'blog', label: 'Blog', position: 'left'},
+                {
+                    href: 'https://mixcore.org',
+                    label: 'Home',
+                    position: 'right',
+                },
+                {
+                    href: 'https://api-docs.mixcore.org',
+                    label: 'API Docs',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/mixcore/mix.core/discussions',
+                    label: 'Discussions',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/mixcore/mix.core/releases',
+                    label: 'Download',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/mixcore/mix.core',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          href: 'https://api-docs.mixcore.org',
-          label: 'API Docs',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/mixcore/mix.core/discussions',
-          label: 'Discussions',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/mixcore/mix.core/releases',
-          label: 'Download',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/mixcore/mix.core',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+        footer: {
+            style: 'dark',
+            links: [{
+                    title: 'Docs',
+                    items: [{
+                            label: 'Style Guide',
+                            to: 'docs/doc1',
+                        },
+                        {
+                            label: 'Second Doc',
+                            to: 'docs/doc2',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [{
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/mixcore',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.gg/67G5JZc',
+                        },
+                    ],
+                },
+                {
+                    title: 'Social',
+                    items: [{
+                            label: 'Blog',
+                            // to: 'blog',
+                            href: 'https://medium.com/mixcore',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/mixcore/mix.core',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/mixcore_cms',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Mixcore CMS.`,
+        }
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    path: 'docs',
+                    routeBasePath: '/',
+                    editUrl: 'https://github.com/mixcore/docs/edit/master/website/',
+                    showLastUpdateAuthor: true,
+                    showLastUpdateTime: true,
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+                sitemap: {},
+                gtag: {
+                    trackingID: 'UA-134154017-3',
+                    anonymizeIP: true,
+                },
             },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/mixcore',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/67G5JZc',
-            },
-          ],
-        },
-        {
-          title: 'Social',
-          items: [
-            {
-              label: 'Blog',
-              // to: 'blog',
-              href: 'https://medium.com/mixcore',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/mixcore/mix.core',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/mixcore_cms',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Mixcore CMS.`,
-    }
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          path: 'docs',
-          routeBasePath: '/',
-          editUrl:
-            'https://github.com/mixcore/docs/edit/master/website/',
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-        sitemap: {},
-        gtag: {
-          trackingID: 'UA-134154017-3',
-          anonymizeIP: true,
-        },
-      },
+        ],
     ],
-  ],
 };
